90356173d138abcc671d2b22f08e39b2ea42f4c9
song windows <dkclasltmf@naver.com>
2023-11-28 22:54:49 +0900

    행렬의덧셈

---
+++ src/main/java/행렬의덧셈.java
@@ -0,0 +1,27 @@
+public class 행렬의덧셈 {
+    public static void main(String[] args) {
+        int[][] arr1 = {{1, 2}, {2, 3}};
+        int[][] arr2 = {{3, 4}, {5, 6}};
+
+        int[][] solution = solution(arr1, arr2);
+
+        for (int i = 0; i < solution.length; i++) {
+            for (int j = 0; j < solution[i].length; j++) {
+                System.out.print(solution[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
+    private static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
